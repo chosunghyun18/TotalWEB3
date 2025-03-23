8eeb1fa4b5e819fb26d47b6345a773d06dee7212
chosunghyun18 <krismon@naver.com>
2025-03-23 16:52:11 +0900

    change token name and tags

--- Dapp/NFT Market/MintAnimalToken.sol
+++ Dapp/NFT Market/MintAnimalToken.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract MintAnimalToken is ERC721Enumerable {
-    constructor() ERC721("h662Animals", "HAS") {}
+    constructor() ERC721("ChoAnimals", "CAS") {}
 
     function mintAnimalToken() public {
         uint256 animalTokenId = totalSupply() + 1;
