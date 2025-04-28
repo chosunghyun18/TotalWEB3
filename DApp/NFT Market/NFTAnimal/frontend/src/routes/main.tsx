c2bd1d502113a98e1f7d2f7d540c297d00513351
chosunghyun18 <krismon@naver.com>
2025-04-28 22:01:21 +0900

    feat : add test rendering page]

--- DApp/NFT Market/NFTAnimal/frontend/src/routes/main.tsx
+++ DApp/NFT Market/NFTAnimal/frontend/src/routes/main.tsx
@@ -1,8 +1,16 @@
 import React, { FC } from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const Main: FC = () => {
-  return <Box>Main</Box>;
+  const navigate = useNavigate();
+
+  return (
+    <Box>
+      Main
+      <Button onClick={() => navigate("/test")}>test</Button>
+    </Box>
+  );
 };
 
 export default Main;
