e0906eb3aac90547ffb15a63172ee6f7dc6c5c2a
chosunghyun18 <krismon@naver.com>
2025-04-28 21:20:22 +0900

    build : add charkra ui

--- DApp/NFT Market/NFTAnimal/frontend/src/index.tsx
+++ DApp/NFT Market/NFTAnimal/frontend/src/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+// null 체크 추가
+const container = document.getElementById("root");
+if (!container) throw new Error('Root element not found');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
